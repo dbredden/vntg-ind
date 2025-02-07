1b5d9e61386487ffc9bca466088caef122af11fe
Danny Redden <dredden@allegion.com>
2025-02-07 16:01:16 -0500

    updated sign up component + authentication route

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 import {
     signInWithGooglePopup,
-    createUserDocumentFromAuth
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword
   } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
@@ -27,18 +28,29 @@ const SignInForm = () => {
         setFormFields(defaultFormFields);
     }
 
-    const logGoogleUser = async () => {
+    const signInWithGoogle = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
+        await createUserDocumentFromAuth(user);
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
         try {
-            // call Firebase docs for functions to check Firestore for this user
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+            resetFormFields();
         } catch (error) {
-            console.log('error logging into account.', error);
+          switch (error.code) {
+            case 'auth/wrong-password':
+              alert('Incorrect password for email');
+              break;
+            case 'auth/user-not-found':
+              alert('no user associated with this email');
+              break;
+            default:
+              console.log(error);
+          }
         }
     };
 
@@ -52,7 +64,7 @@ const SignInForm = () => {
         <div className='sign-in-container'>
             <h2>I already have an account</h2>
             <span>Sign in with your email and password</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FormInput
                     label="Email"
                     type="email"
@@ -69,8 +81,10 @@ const SignInForm = () => {
                     name="password"
                     value={password}
                 />
-                <Button buttonType='inverted' type='submit'>Sign In</Button>
-                <Button buttonType='google' onClick={logGoogleUser}>Sign in With Google</Button>
+                <div className='buttons-container'>
+                    <Button type='submit'>Sign In</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                </div>
             </form>
         </div>
     );
